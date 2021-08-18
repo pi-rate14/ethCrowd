6ffde46016cf0ed54f22c3454c0fb4365d7d81bb
apoorva <apoorva.srivastava2019@vitstudent.ac.in>
2021-08-19 01:22:46 +0530

    added API key to env

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,14 +1,15 @@
-import firebase from 'firebase'
+import firebase from "firebase";
+import { API_KEY } from "./api";
 const firebaseConfig = {
-  apiKey: 'AIzaSyCe2aLuptppdFCKJFjcSej6K99GfU6pEhU',
-  authDomain: 'ethcrowd-f9bdd.firebaseapp.com',
-  projectId: 'ethcrowd-f9bdd',
-  storageBucket: 'ethcrowd-f9bdd.appspot.com',
-  messagingSenderId: '659500728423',
-  appId: '1:659500728423:web:56dd4cd1ccf0b1b5b214c2',
-  measurementId: 'G-KHSTT68JPG',
-}
+  apiKey: API_KEY,
+  authDomain: "ethcrowd-f9bdd.firebaseapp.com",
+  projectId: "ethcrowd-f9bdd",
+  storageBucket: "ethcrowd-f9bdd.appspot.com",
+  messagingSenderId: "659500728423",
+  appId: "1:659500728423:web:56dd4cd1ccf0b1b5b214c2",
+  measurementId: "G-KHSTT68JPG",
+};
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
-export default firebase
+export default firebase;
